768983a90274c8301f20ae8348f136ae48e63874
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-18 14:58:22 -0800

    under indented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -24,41 +24,32 @@ def to_do(employee_ID):
     todos_url = f"{url}/todos?userId={employee_ID}"
 
     employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
 
     if employee_response.status_code == 200:
-        employee_data = employee_response.json()
         employee_name = employee_data.get('name')
 
-        todos_response = requests.get(todos_url)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-        if todos_response.status_code == 200:
-            todos_data = todos_response.json()
-            total_tasks = len(todos_data)
-            completed_tasks = 0
+    if todos_response.status_code == 200:
+        total_tasks = len(todos_data)
+        completed_tasks = 0
+    for task in todos_data:
+        completed_tasks += task['completed']
 
-            for task in todos_data:
-                completed_tasks += task['completed']
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
 
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(employee_name, completed_tasks, total_tasks))
-
-            for task in todos_data:
-                if task['completed']:
-                    print("\t {}".format(task['title']))
-        else:
-            print(f"Error{employee_ID}")
-    else:
-        print(f"Error{employee_ID}")
+    for task in todos_data:
+        if task['completed']:
+            print("\t {}".format(task['title']))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Error")
+        print("error")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-        to_do(employee_id)
-    except ValueError:
-        print("Error")
-        sys.exit(1)
+    employee_id = int(sys.argv[1])
+    to_do(employee_id)
