63a59fac3c03f1284753cd6d032ebbb4d5d260da
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-18 04:36:13 -0800

    Fix: add request

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,22 +3,19 @@
 
 
 import requests
-from sys import argv
+import sys
 
-
-def get_employee_todo_progress(employee_id):
-    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    response = requests.get(api_url)
-    todos = response.json()
-    employee_name = todos[0].get('name')
-    tasks = []
-    for todo in todos:
-        if todo.get('completed'):
-            tasks.append(todo.get('title'))
-    print(f'Employee {employee_name} is done with tasks({len(tasks)}/{len(todos)}):')
-    for task in tasks:
-        print('\t {}'.format(task))
-
-
-if __name__ == '__main__':
-    get_employee_todo_progress(argv[1])
+if len(sys.argv) == 2:
+    user_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(user_id)).json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(user_id)).json()
+    done_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            done_tasks.append(task)
+    print('Employee {} is done with tasks({}/{}):'
+            .format(user.get('name'), len(done_tasks), len(todo)))
+    for task in done_tasks:
+        print('\t {}'.format(task.get('title')))
