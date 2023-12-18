de4982132bc4e57aa6b69bd9fb0eed5de2b1d8c3
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-18 04:08:15 -0800

    Total reformulation of code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,16 +6,19 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = argv[1]
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    todo = requests.get(url + "todos", params={"userId": user_id}).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get("completed") is True:
-            completed_tasks.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed_tasks), len(todo)))
-    for task in completed_tasks:
-        print("\t {}".format(task))
+def get_employee_todo_progress(employee_id):
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(api_url)
+    todos = response.json()
+    employee_name = todos[0].get('name')
+    tasks = []
+    for todo in todos:
+        if todo.get('completed'):
+            tasks.append(todo.get('title'))
+    print(f'Employee {employee_name} is done with tasks({len(tasks)}/{len(todos)}):')
+    for task in tasks:
+        print('\t {}'.format(task))
+
+
+if __name__ == '__main__':
+    get_employee_todo_progress(argv[1])
