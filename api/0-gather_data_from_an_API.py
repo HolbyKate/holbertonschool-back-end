a4c72fb34f94920ed71db9bff7336f3dfeb940c6
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-18 15:09:30 -0800

    change change change

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,39 +20,35 @@ def to_do(employee_ID):
         Displays the employee's TODO list progress.
     """
     url = 'https://jsonplaceholder.typicode.com'
-    employee_url = f"{url}/users/{employee_id}"
-    todos_url = f"{url}/todos?userId={employee_id}"
+    employee_url = f"{url}/users/{employee_ID}"
+    todos_url = f"{url}/todos?userId={employee_ID}"
 
     employee_response = requests.get(employee_url)
-    if employee_response.status_code != 200:
-        print(f"Error fetching employee data. Status code:
-              {employee_response.status_code}")
-        sys.exit(1)
-
     employee_data = employee_response.json()
-    employee_name = employee_data.get('name')
+
+    if employee_response.status_code == 200:
+        employee_name = employee_data.get('name')
 
     todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print(f"Error fetching TODO list data. Status code:
-              {todos_response.status_code}")
-        sys.exit(1)
-
     todos_data = todos_response.json()
-    total_tasks = len(todos_data)
-    completed_tasks = sum(task['completed'] for task in todos_data)
 
-    print(f"Employee {employee_name} is done with tasks
-          ({completed_tasks}/{total_tasks}): ")
+    if todos_response.status_code == 200:
+        total_tasks = len(todos_data)
+        completed_tasks = 0
+    for task in todos_data:
+        completed_tasks += task['completed']
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
 
     for task in todos_data:
         if task['completed']:
-            print(f"\t{task['title']}")
+            print("\t {}".format(task['title']))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Error: Invalid number of arguments.")
+        print("error")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
